ef7a449cb80813ddc43502fc5ae1a5f3e34b7789
PranavKuramkoteSudhir <139109363+PranavKuramkoteSudhir@users.noreply.github.com>
2023-12-25 21:53:07 -0500

    Update config.py

--- source/config/config.py
+++ source/config/config.py
@@ -5,14 +5,14 @@ config = {
         "bootstrap.servers": "pkc-4r087.us-west2.gcp.confluent.cloud:9092",
         "security.protocol": "SASL_SSL",
         "sasl.mechanisms": "PLAIN",
-        "sasl.username": "6Y7FGE6C2DODEAEL",
-        "sasl.password": "9pmtPyVeAWdDk8W3NKOWsvWBtPzOdHp2B512LrKC48+nyPjHJheC3/Pc3r1EQScf",
+        "sasl.username": "api_keyL",
+        "sasl.password": "secret",
         "session_timeout_ms": 50000
 
     },
     "schema_registry": {
         "schema.registry.url": "https://psrc-5j7x8.us-central1.gcp.confluent.cloud",
         "basic.auth.credentials.source": "USER_INFO",
-        "basic.auth.user.info": "BDH7QXOH5VMH4UEU:uNImLm2pOuf6KV3JewkipxNODIw3jIfPbNRMtN8hEVd5fO6rZvN5oYR3/LzksQUp"
+        "basic.auth.user.info": "api_key:secret"
     }
 }
